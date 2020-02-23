81c8d8a7ce69c2ba6f45e526a6f31e5bec4c70a5
zerinho6 <zerinho6@gmail.com>
2020-02-22 22:18:49 -0300

    Fixed wrong grab

--- src/utils/languageUtils/operationLanguages.js
+++ src/utils/languageUtils/operationLanguages.js
@@ -5,7 +5,7 @@
  */
 exports.profileOperationAllLanguages = () => {
   const Operations = []
-  const { LanguageUtils } = require("./index.js")
+  const LanguageUtils = require("./index.js")
   for (let i = 0; i < LanguageUtils.acceptableLanguages.length; i++) {
     const I18n = LanguageUtils.init(LanguageUtils.acceptableLanguages.length[i])
     Operations.push([I18n.__("Currency_Create"), I18n.__("Currency_Edit"), I18n.__("Currency_Delete")])
